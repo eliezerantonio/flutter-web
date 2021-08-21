881632a7030319c4a31f9540cf128b0563b9c6f0
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-21 06:20:13 +0100

    provider

--- contador/lib/ui/pages/counter_provider_page.dart
+++ contador/lib/ui/pages/counter_provider_page.dart
@@ -1,12 +1,21 @@
+import 'package:contador/providers/counter_provider.dart';
 import 'package:contador/ui/shared/custom_app_menu.dart';
 import 'package:contador/ui/shared/custom_flat_button.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CounterProviderPage extends StatelessWidget {
   int counter = 0;
 
   @override
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => CounterProvider(),
+      child: _counterProviderPageBody(),
+    );
+  }
+
+  Scaffold _counterProviderPageBody() {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
