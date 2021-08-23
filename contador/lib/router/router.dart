32623eb4017d74f972c413083134165b3beb61fc
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-23 05:28:23 +0100

    page 404

--- contador/lib/router/router.dart
+++ contador/lib/router/router.dart
@@ -2,25 +2,32 @@ import 'package:fluro/fluro.dart';
 
 import 'package:contador/ui/views/counter_provider_view.dart';
 import 'package:contador/ui/views/counter_view.dart';
+import 'package:contador/ui/views/view404.dart';
 
 class Flurorouter {
   static final FluroRouter router = new FluroRouter();
 
   static void configureRoutes() {
+    
     router.define(
       '/',
       handler: _counterHandler,
-      transitionType: TransitionType.none,
+      transitionType: TransitionType.fadeIn,
     );
 
-    router.define('/stateful',
-        handler: _counterStatefulHandler, transitionType: TransitionType.none);
+    router.define(
+      '/stateful',
+      handler: _counterHandler,
+      transitionType: TransitionType.fadeIn,
+    );
 
     router.define(
       '/provider',
       handler: _counterProviderHandler,
-      transitionType: TransitionType.none,
+      transitionType: TransitionType.fadeIn,
     );
+
+    router.notFoundHandler = _pageNotFoundHandler;
   }
 
   //handlers
@@ -28,11 +35,12 @@ class Flurorouter {
     handlerFunc: (context, params) => CounterView(),
   );
 
-  static Handler _counterStatefulHandler = new Handler(
-    handlerFunc: (context, params) => CounterView(),
-  );
-
   static Handler _counterProviderHandler = new Handler(
     handlerFunc: (context, params) => CounterProviderView(),
   );
+
+  //404
+
+  static Handler _pageNotFoundHandler =
+      new Handler(handlerFunc: (context, params) => View404());
 }
