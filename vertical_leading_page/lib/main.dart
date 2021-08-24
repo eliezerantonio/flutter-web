b46dc03b2775c7330dd73846a223d429ada48031
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-24 05:42:58 +0100

    configurando rota home

--- vertical_leading_page/lib/main.dart
+++ vertical_leading_page/lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:vertical_leading_page/router/router.dart';
 import 'package:vertical_leading_page/ui/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    Flurorouter.configureRoutes();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vertical Scrolling',
-      debugShowCheckedModeBanner:false
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/home',
+      onGenerateRoute: Flurorouter.router.generator,
     );
   }
 }
--- vertical_leading_page/lib/ui/pages/home_page.dart
+++ vertical_leading_page/lib/ui/pages/home_page.dart
@@ -4,10 +4,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: 100,
       color: Colors.red,
-      child: Text("Eliezer"),
     );
   }
 }
