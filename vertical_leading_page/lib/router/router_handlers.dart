cb301bfcc0c35c7424be8502943c245b09f90dbd
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-24 05:39:37 +0100

    sistema de navegacao

---
+++ vertical_leading_page/lib/router/router_handlers.dart
@@ -0,0 +1,8 @@
+import 'package:fluro/fluro.dart';
+import 'package:vertical_leading_page/ui/pages/home_page.dart';
+
+final homeHandler = Handler(handlerFunc: (context, params) {
+  return HomePage();
+});
+
+
---
+++ vertical_leading_page/lib/ui/pages/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 100,
+      color: Colors.red,
+      child: Text("Eliezer"),
+    );
+  }
+}
