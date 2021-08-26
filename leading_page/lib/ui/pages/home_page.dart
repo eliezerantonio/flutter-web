c360f318bf88c3f98df0f5eebcdd37d03ed66e1d
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-26 21:45:29 +0100

    ajustando gradiante

--- leading_page/lib/ui/pages/home_page.dart
+++ leading_page/lib/ui/pages/home_page.dart
@@ -1,36 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:leading_page/ui/views/about_view.dart';
+import 'package:leading_page/ui/views/contact_view.dart';
 import 'package:leading_page/ui/views/home_view.dart';
+import 'package:leading_page/ui/views/location_view.dart';
+import 'package:leading_page/ui/views/pricing_view.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Stack(
-          children: [
-            _HomeBody(),
-            Positioned(
-              right: 20,
-              top: 20,
-              child: Container(
-                width: 150,
-                height: 50,
-                color: Colors.black,
-              ),
-            )
-          ],
-        ),
+        decoration: buildBoxDecoration(),
+        child: Stack(children: [
+          _HomeBody(),
+          Positioned(
+            right: 20,
+            top: 20,
+            child: Container(
+              width: 150,
+              height: 50,
+              color: Colors.black,
+            ),
+          )
+        ]),
       ),
     );
   }
+
+  BoxDecoration buildBoxDecoration() => BoxDecoration(
+        gradient: LinearGradient(
+            colors: [
+              Colors.pink,
+              Colors.deepPurple,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.5, 0.5]),
+      );
 }
 
 class _HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
+      physics: BouncingScrollPhysics(),
+      scrollDirection: Axis.vertical,
       children: [
         HomeView(),
+        AboutView(),
+        PricingView(),
+        ContactView(),
+        LocationView()
       ],
     );
   }
