8e4553cd11ce2294ab4889ec654eb5c094455fcb
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-27 06:13:50 +0100

    mudando cursor

--- leading_page/lib/ui/shared/custom_app_menu.dart
+++ leading_page/lib/ui/shared/custom_app_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomAppMenu extends StatelessWidget {
@@ -6,26 +7,32 @@ class CustomAppMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 150,
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      height: 50,
-      color: Colors.black,
-      child: Row(
-        children: [
-          Text(
-            "Menu",
-            style: GoogleFonts.roboto(
-              color: Colors.white,
-              fontSize: 18,
-            ),
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          width: 150,
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          height: 50,
+          color: Colors.black,
+          child: Row(
+            children: [
+              Text(
+                "Menu",
+                style: GoogleFonts.roboto(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+              Spacer(),
+              Icon(
+                Icons.menu,
+                color: Colors.white,
+              )
+            ],
           ),
-          Spacer(),
-          Icon(
-            Icons.menu,
-            color: Colors.white,
-          )
-        ],
+        ),
       ),
     );
   }
