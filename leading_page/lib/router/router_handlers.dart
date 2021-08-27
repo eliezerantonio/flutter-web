4a0655b953bda3c081f7dd34650c6d8318bfb230
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-27 12:17:45 +0100

    url

--- leading_page/lib/router/router_handlers.dart
+++ leading_page/lib/router/router_handlers.dart
@@ -1,7 +1,15 @@
 import 'package:fluro/fluro.dart';
+import 'package:leading_page/providers/page_provider.dart';
 import 'package:leading_page/ui/pages/home_page.dart';
+import 'package:provider/provider.dart';
 
-final homeHandler = Handler(handlerFunc: (context, params) {
-  final page = params['page']!.first;
-  if (page != '/') return HomePage();
-});
+final homeHandler = Handler(
+  handlerFunc: (context, params) {
+    final page = params['page']!.first;
+    if (page != '/') {
+      final pageProvider = Provider.of<PageProvider>(context!, listen: false);
+      pageProvider.createScrollController(page);
+      return HomePage();
+    }
+  },
+);
