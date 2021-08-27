1b16a4680ae020e0273c5828b4f3a68a89384da0
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-27 06:31:00 +0100

    animando

--- leading_page/lib/ui/shared/custom_app_menu.dart
+++ leading_page/lib/ui/shared/custom_app_menu.dart
@@ -2,22 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class CustomAppMenu extends StatelessWidget {
-  const CustomAppMenu({Key? key}) : super(key: key);
+class CustomAppMenu extends StatefulWidget {
+  @override
+  _CustomAppMenuState createState() => _CustomAppMenuState();
+}
+
+class _CustomAppMenuState extends State<CustomAppMenu>
+    with SingleTickerProviderStateMixin {
+  bool isOpen = false;
+
+  late AnimationController controller;
+  @override
+  void initState() {
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
       cursor: SystemMouseCursors.click,
       child: GestureDetector(
-        onTap: () {},
+        onTap: () {
+          if (isOpen) {
+            controller.reverse();
+          } else {
+            controller.forward();
+          }
+          setState(() {
+            isOpen = !isOpen;
+          });
+        },
         child: Container(
-          width: 150,
           padding: EdgeInsets.symmetric(horizontal: 10),
+          width: 150,
           height: 50,
           color: Colors.black,
           child: Row(
             children: [
+              AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                curve: Curves.easeInOut,
+                width: isOpen ? 50 : 0,
+              ),
               Text(
                 "Menu",
                 style: GoogleFonts.roboto(
@@ -26,9 +54,10 @@ class CustomAppMenu extends StatelessWidget {
                 ),
               ),
               Spacer(),
-              Icon(
-                Icons.menu,
+              AnimatedIcon(
+                icon: AnimatedIcons.close_menu,
                 color: Colors.white,
+                progress: controller,
               )
             ],
           ),
