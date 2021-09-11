7b2507fddfc1fcd5f9e39f2ed9d23e6d844cacc4
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-11 23:55:58 +0100

    auhtlayout

---
+++ dashboard/lib/ui/layout/auth/auth_layout.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AuthLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text(
+            "Auth layout",
+          ),
+        ),
+      ),
+    );
+  }
+}
