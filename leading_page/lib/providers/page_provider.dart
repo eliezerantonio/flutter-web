90292f7ab95687b1a62068971bf5737350c11ae8
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-11 22:32:35 +0100

    leading page done

--- leading_page/lib/providers/page_provider.dart
+++ leading_page/lib/providers/page_provider.dart
@@ -5,10 +5,25 @@ class PageProvider with ChangeNotifier {
   PageController scrolConroller = new PageController();
 
   List<String> _pages = ['home', 'about', 'pricing', 'contact', 'location'];
+  int _currentIndex = 0;
 
   createScrollController(String routerName) {
     this.scrolConroller =
         new PageController(initialPage: getPageIndex(routerName));
+    html.document.title = _pages[getPageIndex(routerName)];
+
+    scrolConroller.addListener(() {
+      final index = (this.scrolConroller.page ?? 0).round();
+
+      if (index != _currentIndex) {
+        final routeName = _pages[index];
+
+        html.window.history.pushState(null, '', '#/$routeName');
+
+        _currentIndex = index;
+        html.document.title = _pages[index];
+      }
+    });
   }
 
   int getPageIndex(String routeName) {
