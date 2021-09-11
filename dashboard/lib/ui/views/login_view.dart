2551c76691874e0fe93a6d93847f567bbb6fb700
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-11 23:56:53 +0100

    login view

---
+++ dashboard/lib/ui/views/login_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          "LoginView",
+        ),
+      ),
+    );
+  }
+}
