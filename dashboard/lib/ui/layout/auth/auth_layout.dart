cdd8acc68cb31a4c014a05a830dea287e37fb326
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-09-18 17:51:40 +0100

    BackgroundTwitter

--- dashboard/lib/ui/layout/auth/auth_layout.dart
+++ dashboard/lib/ui/layout/auth/auth_layout.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/background_twitter.dart';
+
 class AuthLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text(
-            "Auth layout",
-          ),
-        ),
-      ),
-    );
+        body: ListView(
+      children: [
+//Desktop
+        _DesktopBody()
+
+//Mobile
+
+//LinksBar
+      ],
+    ));
+  }
+}
+
+class _DesktopBody extends StatelessWidget {
+  const _DesktopBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+        width: size.width,
+        height: size.height,
+        color: Colors.red,
+        child: Row(
+          children: [
+            //Twiter background
+            BackgroundTwitter(),
+            //View container background
+            Container(
+              width: 600,
+              height: double.infinity,
+              color: Colors.black,
+            )
+          ],
+        ));
   }
 }
